6d1cb968cde0ce16b6e3fc8a8f05941831bf1234
Julia Jodczyk <julia.jodczyk@gmail.com>
2025-12-06 22:24:27 +0100

    integrates map to current state

--- fe/lib/date_page.dart
+++ fe/lib/date_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'map_view.dart';
 
 class DatePage extends StatelessWidget {
   const DatePage({super.key});
@@ -17,6 +18,22 @@ class DatePage extends StatelessWidget {
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 8),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.map_outlined),
+            label: const Text('Start Your Bydgoszcz Date Route!'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MapScreen(),
+                ),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              textStyle: const TextStyle(fontSize: 18),
+            ),
+          ),
         ],
       ),
     );
