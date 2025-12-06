39b323957a9d6fd78663eef533d29d8ac4967f1c
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-12-06 23:11:56 +0100

    next user, swipe

--- be/app/routers/users.py
+++ be/app/routers/users.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Annotated, TypedDict
 
-from fastapi import APIRouter, Depends, HTTPException, Header
+from fastapi import APIRouter, Depends, Header, HTTPException, Response
 from pydantic import BaseModel
 from pymongo.database import Collection
 
@@ -31,8 +31,19 @@ class UserRes(BaseModel):
         )
 
 
+class UserLink(TypedDict):
+    login_1: str
+    login_2: str
+    approved_1: bool | None
+    approved_2: bool | None
+
+
 def get_user_db(db: Annotated[Db, Depends(get_db)]) -> Collection[User]:
     return db["users"]
+
+
+def get_user_link_db(db: Annotated[Db, Depends(get_db)]) -> Collection[UserLink]:
+    return db["user_links"]
 
 
 def get_me(
@@ -57,3 +68,80 @@ async def read_me(
     me: Annotated[User, Depends(get_me)],
 ) -> UserRes:
     return UserRes.from_domain(me)
+
+
+@router.get("/users/next")
+async def read_next(
+    me: Annotated[User, Depends(get_me)],
+    users: Annotated[Collection[User], Depends(get_user_db)],
+    user_links: Annotated[Collection[UserLink], Depends(get_user_link_db)],
+) -> UserRes:
+    my_login = me["login"]
+    considered = user_links.find(
+        {
+            "$or": [
+                {"login_1": my_login, "approved_1": {"$ne": None}},
+                {"login_1": my_login, "approved_2": False},
+                {"login_2": my_login, "approved_2": {"$ne": None}},
+                {"login_2": my_login, "approved_1": False},
+            ]
+        }
+    )
+    considered_logins = [
+        login for link in considered for login in [link["login_1"], link["login_2"]]
+    ]
+    considered_logins.append(my_login)
+    filters = [{"login": {"$ne": login}} for login in considered_logins]
+    if len(filters) > 0:
+        unconsidered = users.aggregate(
+            [{"$match": {"$and": filters}}, {"$sample": {"size": 1}}]
+        )
+    else:
+        unconsidered = users.aggregate([{"$sample": {"size": 1}}])
+
+    other = next(unconsidered, None)
+    if other is None:
+        raise HTTPException(404, "Not found")
+
+    return UserRes.from_domain(other)
+
+
+@router.put("/users/{login}/swipe")
+async def swipe(
+    me: Annotated[User, Depends(get_me)],
+    login: str,
+    matched: bool,
+    users: Annotated[Collection[User], Depends(get_user_db)],
+    user_links: Annotated[Collection[UserLink], Depends(get_user_link_db)],
+):
+    other = users.find_one({"login": login})
+    if other == None:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    my_login = me["login"]
+    other_login = other["login"]
+
+    link_filter = {
+        "$or": [
+            {"login_1": my_login, "login_2": other_login},
+            {"login_1": other_login, "login_2": my_login},
+        ]
+    }
+    link = user_links.find_one(link_filter)
+    if link is None:
+        link = UserLink(
+            {
+                "login_1": my_login,
+                "login_2": other["login"],
+                "approved_1": None,
+                "approved_2": None,
+            }
+        )
+
+    if link["login_1"] == my_login:
+        link["approved_1"] = matched
+    else:
+        link["approved_2"] = matched
+
+    _ = user_links.update_one(link_filter, {"$set": link}, upsert=True)
+    return Response()
